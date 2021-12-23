2bd964c242b4c442cd4b1dfafd729bbe975247cc
Bashir Yesufu <yesufubashir@gmail.com>
2021-12-23 16:55:17 +0100

    Added the content of the code verification bottomsheet

--- bet_run_app/lib/screens/login_signup/code_verification_bottomsheet.dart
+++ bet_run_app/lib/screens/login_signup/code_verification_bottomsheet.dart
@@ -1,3 +1,4 @@
+import 'package:bet_run_app/reuseables/components.dart';
 import 'package:bet_run_app/reuseables/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,42 @@ class CodeVerificationBottomsheet extends StatelessWidget {
       margin: EdgeInsets.symmetric(vertical: 30.0),
       height: (MediaQuery.of(context).size.height) * 0.7,
       decoration: kRoundBottomSheet,
+      child: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20.0,
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TitleText(
+                    text: 'Enter the verification code \nwe sent you',
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                ],
+              ),
+              // Spacer(),
+              WhiteButton(
+                buttonText: 'Back',
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                buttonIcon: '',
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
