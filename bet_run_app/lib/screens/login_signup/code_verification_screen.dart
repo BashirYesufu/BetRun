adc997f670549fd73fc0f7af5402fd28d06bdad3
Bashir Yesufu <yesufubashir@gmail.com>
2021-12-23 16:30:00 +0100

    Added a code verification screen

---
+++ bet_run_app/lib/screens/login_signup/code_verification_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CodeVerificationScreen extends StatelessWidget {
+  const CodeVerificationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
